496f3a9cadf7a0552ca30107ec08c89d9ed3415f
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2020-04-19 20:43:57 -0400

    adição de novas funções

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 function insert(num){
     document.form.textview.value = document.form.textview.value + num;
-}     
-function equal(){
+}  
+function preview(){
     var exp = document.form.textview.value;
     if(exp){
-        document.form.textview.value = eval(exp);
+        var res = document.form.preview.value = eval(exp);
+        return res
     }
+}
+function equal(){
+    document.form.textview.value = preview();
+    cleanPreview();
+}
+function cleanPreview(){
+    document.form.preview.value = '';
 }
 function clean(){
     document.form.textview.value = '';
+    cleanPreview();
 }
 function back(){
     var exp = document.form.textview.value;
     document.form.textview.value = exp.substring(0,exp.length-1);
+    document.form.preview.value = '';
 }
